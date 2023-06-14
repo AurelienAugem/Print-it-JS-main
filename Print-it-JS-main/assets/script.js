bd6f5798254bc860e600d24ca1855377bc1d187b
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-06-14 10:46:09 +0200

    fleches et bullet points

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -11,7 +11,7 @@ function testRightArrow(){
 /** Event listener sur les boutons */
 leftArrow.addEventListener('click', testLeftArrow);
 rightArrow.addEventListener('click', testRightArrow);
-
+/** Bullet points */
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -30,3 +30,18 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/** Varaible récupérant l'élément ayant la classe CSS "dots" contenant les bullet points*/
+var bulletPoints = document.querySelector('.dots');
+var slidePoints = document.querySelector('.banner-img');
+/** Affichage des bullet Points en fonction du nombre de slides */
+for(let i=0; i < slides.length; i++){
+
+	/** Création d'un <span> */
+	const bPoint = document.createElement("span");
+	/** La classe "dot" est associé à l'élément précédemment créer */
+	bPoint.className = "dot";
+	/** Le <span> est positionné dans l'élément parent  */
+	bulletPoints.appendChild(bPoint);
+
+}
