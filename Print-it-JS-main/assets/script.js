9e1d1ee39eead68a0c1470467fbed3b0ff27c284
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-06-14 16:51:18 +0200

    génération de code html

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,16 +1,4 @@
-/** Récupération des flèches du slider dans des variables */
-var leftArrow = document.querySelector('#banner .arrow_left');
-var rightArrow = document.querySelector('#banner .arrow_right');
-/** Fonction de test de fonctionnement des flèches */
-function testLeftArrow(){
-	console.log("Flèche gauche fonctionne");
-}
-function testRightArrow(){
-	console.log("Flèche droite fonctionne");
-}
-/** Event listener sur les boutons */
-leftArrow.addEventListener('click', testLeftArrow);
-rightArrow.addEventListener('click', testRightArrow);
+
 /** Bullet points */
 const slides = [
 	{
@@ -31,9 +19,40 @@ const slides = [
 	}
 ]
 
+/** Récupération de la bannière */
+let mainBanner = document.querySelector('#banner');
+mainBanner.innerHTML = "";
+
+/** Génération des flèches de navigation */
+const slideLeft = document.createElement('input');
+slideLeft.setAttribute("type", "image");
+slideLeft.setAttribute("src","./assets/images/arrow_left.png");
+slideLeft.className = "arrow arrow_left";
+mainBanner.appendChild(slideLeft);
+const slideRight = document.createElement('input');
+slideRight.setAttribute("type","image");
+slideRight.setAttribute("src","./assets/images/arrow_right.png");
+slideRight.className = "arrow arrow_right";
+mainBanner.appendChild(slideRight);
+
+/** Génération de la div contenant les bullet points */
+const slideDots = document.createElement('div');
+slideDots.className = "dots";
+mainBanner.appendChild(slideDots);
+
+const slideImage = document.createElement('img');
+slideImage.setAttribute("alt", "Banner Print-it");
+slideImage.className = "banner-img";
+mainBanner.appendChild(slideImage);
+
+const slideText = document.createElement('p');
+slideImage.appendChild(slideText);
+
+
+
 /** Varaible récupérant l'élément ayant la classe CSS "dots" contenant les bullet points*/
-var bulletPoints = document.querySelector('.dots');
-var slidePoints = document.querySelector('.banner-img');
+let bulletPoints = document.querySelector('.dots');
+
 /** Affichage des bullet Points en fonction du nombre de slides */
 for(let i=0; i < slides.length; i++){
 
@@ -45,3 +64,18 @@ for(let i=0; i < slides.length; i++){
 	bulletPoints.appendChild(bPoint);
 
 }
+
+/** Récupération des flèches du slider dans des variables */
+let leftArrow = document.querySelector('#banner .arrow_left');
+let rightArrow = document.querySelector('#banner .arrow_right');
+/** Fonction de test de fonctionnement des flèches */
+function testLeftArrow(){
+	console.log("Flèche gauche fonctionne");
+}
+function testRightArrow(){
+	console.log("Flèche droite fonctionne");
+}
+
+/** Event listener sur les boutons */
+leftArrow.addEventListener('click', testLeftArrow);
+rightArrow.addEventListener('click', testRightArrow);
